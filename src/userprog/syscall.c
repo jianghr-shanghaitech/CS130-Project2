01af30698833d8fa4b318179cf8bf24d1b411928
jianghr <jianghr@shanghaitech.edu.cn>
2022-11-04 15:41:21 +0800

    case

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,12 +17,10 @@
 # define USER_VADDR_BOUND (void*) 0x08048000
 static void (*syscalls[15])(struct intr_frame *);
 
-/* Our implementation for Task2: syscall halt,exec,wait and practice */
 void halt(); /* syscall halt. */
-void exit(int status); /* syscall exit. */
+void exit(uint32_t * f); /* syscall exit. */
 void exec(struct intr_frame* f); /* syscall exec. */
 
-/* Our implementation for Task3: syscall create, remove, open, filesize, read, write, seek, tell, and close */
 void create(struct intr_frame* f); /* syscall create */
 void remove(struct intr_frame* f); /* syscall remove */
 void open(struct intr_frame* f);/* syscall open */
@@ -41,7 +39,19 @@ void
 syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
-
+  syscalls[SYS_HALT] = &halt;
+  syscalls[SYS_EXIT] = &exit;
+  syscalls[SYS_EXEC] = &exec;
+  syscalls[SYS_WAIT] = &wait;
+  syscalls[SYS_CREATE] = &create;
+  syscalls[SYS_REMOVE] = &remove;
+  syscalls[SYS_OPEN] = &open;
+  syscalls[SYS_WRITE] = &write;
+  syscalls[SYS_SEEK] = &seek;
+  syscalls[SYS_TELL] = &tell;
+  syscalls[SYS_CLOSE] =&close;
+  syscalls[SYS_READ] = &read;
+  syscalls[SYS_FILESIZE] = &filesize;
 }
 
 static int 
@@ -63,7 +73,8 @@ void * ptr2(const void *vaddr)
   uint8_t i = 0;
   while (i < 4) 
   {
-    if (get_user(check_byteptr + i) == -1)
+    auto byteptr = get_user(check_byteptr + i);
+    if (byteptr == -1)
     {
       thread_current()->exit_status = -1;
       thread_exit ();
@@ -75,32 +86,28 @@ void * ptr2(const void *vaddr)
 
 
 void 
-halt ()
-{
-  // Terminates Pintos
-  shutdown_power_off();
-}
+halt () {shutdown_power_off();}
 
 void 
-exit (int status)
+exit (uint32_t * f)
 {
-  thread_current()->exit_status = status;
-  // Terminates the current user program
+  uint32_t *user_ptr = f;
+  ptr2 (user_ptr + 1);
+  *user_ptr++;
+  thread_current()->exit_status = *user_ptr;
   thread_exit ();
 }
 
-/* Do sytem exec */
 void 
 exec (struct intr_frame* f)
 {
   uint32_t *user_ptr = f->esp;
-  ptr2 (user_ptr + 1);
+  ptr2 (f->esp + 1);
   ptr2 (*(user_ptr + 1));
   *user_ptr++;
   f->eax = process_execute((char*)* user_ptr);
 }
 
-/* Do sytem wait */
 void 
 wait (struct intr_frame* f)
 {
@@ -110,9 +117,7 @@ wait (struct intr_frame* f)
   f->eax = process_wait(*user_ptr);
 }
 
-/*Our implementation for Task3: create, remove, open, filesize, read, write, seek, tell, and close */
 
-/* Do sytem create, we need to acquire lock for file operation in the following methods when do file operation */
 void 
 create(struct intr_frame* f)
 {
@@ -125,7 +130,6 @@ create(struct intr_frame* f)
   release_lock_f ();
 }
 
-/* Do system remove, by calling the method filesys_remove */
 void 
 remove(struct intr_frame* f)
 {
@@ -138,7 +142,6 @@ remove(struct intr_frame* f)
   release_lock_f ();
 }
 
-/* Do system open, open file by the function filesys_open */
 void 
 open (struct intr_frame* f)
 {
@@ -147,8 +150,8 @@ open (struct intr_frame* f)
   ptr2 (*(user_ptr + 1));
   *user_ptr++;
   acquire_lock_f ();
-  struct file * file_opened = filesys_open((const char *)*user_ptr);
   release_lock_f ();
+  struct file * file_opened = filesys_open((const char *)*user_ptr);
   struct thread * t = thread_current();
   if (file_opened)
   {
@@ -157,13 +160,10 @@ open (struct intr_frame* f)
     thread_file_temp->file = file_opened;
     list_push_back (&t->files, &thread_file_temp->file_elem);
     f->eax = thread_file_temp->fd;
-  } 
-  else
-  {
-    f->eax = -1;
+    return;
   }
+  f->eax = -1;
 }
-/* Do system write, Do writing in stdout and write in files */
 void 
 write (struct intr_frame* f)
 {
@@ -171,28 +171,23 @@ write (struct intr_frame* f)
   ptr2 (user_ptr + 7);
   ptr2 (*(user_ptr + 6));
   *user_ptr++;
-  int temp2 = *user_ptr;
   const char * buffer = (const char *)*(user_ptr+1);
   off_t size = *(user_ptr+2);
-  if (temp2 == 1) {
-    /* Use putbuf to do testing */
+  if (*user_ptr == 1) {
     putbuf(buffer,size);
     f->eax = size;
   }
   else
   {
-    /* Write to Files */
     struct thread_file * thread_file_temp = find_file_id (*user_ptr);
     if (thread_file_temp)
     {
       acquire_lock_f ();
       f->eax = file_write (thread_file_temp->file, buffer, size);
       release_lock_f ();
+      return;
     } 
-    else
-    {
-      f->eax = 0;
-    }
+    f->eax = 0;
   }
 }
 /* Do system seek, by calling the function file_seek() in filesystem */
@@ -211,7 +206,6 @@ seek(struct intr_frame* f)
   }
 }
 
-/* Do system tell, by calling the function file_tell() in filesystem */
 void 
 tell (struct intr_frame* f)
 {
@@ -229,7 +223,6 @@ tell (struct intr_frame* f)
   }
 }
 
-/* Do system close, by calling the function file_close() in filesystem */
 void 
 close (struct intr_frame* f)
 {
@@ -242,32 +235,29 @@ close (struct intr_frame* f)
     acquire_lock_f ();
     file_close (opened_file->file);
     release_lock_f ();
-    /* Remove the opened file from the list */
     list_remove (&opened_file->file_elem);
-    /* Free opened files */
     free (opened_file);
   }
 }
-/* Do system filesize, by calling the function file_length() in filesystem */
+
+
 void 
 filesize (struct intr_frame* f){
   uint32_t *user_ptr = f->esp;
-  ptr2 (user_ptr + 1);
+  ptr2 (f->esp + 1);
   *user_ptr++;
-  struct thread_file * thread_file_temp = find_file_id (*user_ptr);
-  if (thread_file_temp)
+  struct thread_file * file_temp = find_file_id (*user_ptr);
+  auto file = file_temp->file;
+  if (file_temp)
   {
     acquire_lock_f ();
-    f->eax = file_length (thread_file_temp->file);
+    f->eax = file_length (file);
     release_lock_f ();
+    return;
   } 
-  else
-  {
-    f->eax = -1;
-  }
+  f->eax = -1;
 }
 
-/* Check is the user pointer is valid */
 bool 
 is_valid_pointer (void* esp,uint8_t argc){
   for (uint8_t i = 0; i < argc; ++i)
@@ -281,14 +271,11 @@ is_valid_pointer (void* esp,uint8_t argc){
 }
 
 
-/* Do system read, by calling the function file_tell() in filesystem */
 void 
 read (struct intr_frame* f)
 {
   uint32_t *user_ptr = f->esp;
-  /* PASS the test bad read */
   *user_ptr++;
-  /* We don't konw how to fix the bug, just check the pointer */
   int fd = *user_ptr;
   int i;
   uint8_t * buffer = (uint8_t*)*(user_ptr+1);
@@ -297,49 +284,46 @@ read (struct intr_frame* f)
       thread_current()->exit_status = -1;
   thread_exit ();
   }
-  /* get the files buffer */
-  if (fd == 0) 
+  if (!fd) 
   {
     for (i = 0; i < size; i++)
       buffer[i] = input_getc();
     f->eax = size;
+    return;
   }
-  else
+  struct thread_file * thread_file_temp = find_file_id (*user_ptr);
+  auto file = thread_file_temp->file;
+  if (thread_file_temp)
   {
-    struct thread_file * thread_file_temp = find_file_id (*user_ptr);
-    if (thread_file_temp)
-    {
-      acquire_lock_f ();
-      f->eax = file_read (thread_file_temp->file, buffer, size);
-      release_lock_f ();
-    } 
-    else
-    {
-      f->eax = -1;
-    }
-  }
+    acquire_lock_f ();
+    f->eax = file_read (file, buffer, size);
+    release_lock_f ();
+    return;
+  } 
+  f->eax = -1;
 }
 
-/* Find file by the file's ID */
 struct thread_file * 
 find_file_id (int file_id)
 {
   struct list_elem *e;
   struct thread_file * thread_file_temp = NULL;
   struct list *files = &thread_current ()->files;
-  for (e = list_begin (files); e != list_end (files); e = list_next (e)){
+  e = list_begin (files);
+  while ( e != list_end (files))
+  {
     thread_file_temp = list_entry (e, struct thread_file, file_elem);
-    if (file_id == thread_file_temp->fd)
-      return thread_file_temp;
+    auto fd = thread_file_temp->fd;
+    if (file_id == fd)
+    return thread_file_temp;
+    e = list_next (e);
   }
   return false;
 }
 
-/* Smplify the code to maintain the code more efficiently */
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  /* For Task2 practice, just add 1 to its first argument, and print its result */
   int * p = f->esp;
   check_ptr2 (p + 1);
   switch(*(int *)f->esp){
@@ -364,9 +348,16 @@ syscall_handler (struct intr_frame *f UNUSED)
       exit_special ();
       break;
   }
+  // int * p = f->esp;
+  // ptr2 (p + 1);
   // int type = * (int *)f->esp;
-  // if(type <= 0 || type >= max_syscall){
-  //   exit_special ();
+  // if(type <= 0 || type >= 15){
+  //   thread_current()->exit_status = -1;
+  //   thread_exit ();
+  // }
+  // if (type == 1)
+  // {
+  //   syscalls[type](f->esp);
   // }
   // syscalls[type](f);
 }
